ea7769792bd976de90ab6d52e3a1126ea521bda8
Jashan190 <singhjashandeep190@gmail.com>
2025-06-29 22:19:58 -0700

    icon change

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import PrayerTimes from "./components/PrayerTimes";  // ← your popup component
+import PrayerTimes from "./components/PrayerTimes";  
 import "./components/PrayerTimes.css"; 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
